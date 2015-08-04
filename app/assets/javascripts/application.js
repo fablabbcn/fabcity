fa0374831f7eaf278e615507a374427713e44224
John Rees <john@bitsushi.com>
2015-08-05 00:15:04 +0100

    css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,8 @@ $(document).ready(function() {
 if ($('#earth_div').length > 0) {
   var options = {atmosphere: true, center: [0, 0], zoom: 8};
   var earth = new WE.map('earth_div', {minAltitude: 40000, maxAltitude: 10000000});
-  WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  WE.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+      id: 'johnrees.n3940g45'
   }).addTo(earth);
 
   var marker = WE.marker([41.3909267,2.1673073]).addTo(earth);
