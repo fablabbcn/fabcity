d6f4d2389834ff77ef856a1eeb4eee1ac84ac286
John Rees <john@bitsushi.com>
2015-08-04 09:13:49 +0100

    styles and countdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,39 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.countdown
 //= require turbolinks
 //= require_tree .
 
-var map = L.map('map').setView([51.505, -0.09], 3);
+var map = L.map('map').setView([51.505, -0.09], 2);
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
   maxZoom: 18,
-  id: 'examples.map-i875mjb7'
+  id: 'eleanor.ipncow29'
 }).addTo(map);
 
-$.getJSON( "https://new-api.smartcitizen.me/v0/devices/world_map", function( data ) {
-  for (var i = 0; i < data.length; i++) {
-    var item = data[i];
-    if ( item.latitude && item.longitude ) {
-      L.marker([item.latitude, item.longitude]).addTo(map).bindPopup('<a href="/v0/devices/' + item.id + '">' + item.id + '</a>: ' + item.name);
+// $.getJSON( "https://new-api.smartcitizen.me/v0/devices/world_map", function( data ) {
+//   for (var i = 0; i < data.length; i++) {
+//     var item = data[i];
+//     if ( item.latitude && item.longitude ) {
+//       L.marker([item.latitude, item.longitude]).addTo(map).bindPopup('<a href="/v0/devices/' + item.id + '">' + item.id + '</a>: ' + item.name);
+//     }
+//   }
+// });
+
+      var isWebkit = 'WebkitAppearance' in document.documentElement.style;
+
+
+$(function() {
+
+var endDate;
+
+  date = new Date("July 7, 2054 18:16:30");
+
+  $('.countdown.styled').countdown({
+    date: date,
+    render: function(data) {
+      $(this.el).html("<div>" + this.leadingZeros(data.years, 2) + " <span>years</span></div><div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hours</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>minutes</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>seconds</span></div>");
     }
-  }
+  });
+
 });
